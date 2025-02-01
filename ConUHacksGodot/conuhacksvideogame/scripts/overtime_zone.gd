acf851ca759ffea4b34f97b188da3e8edfb37a6e
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-02-01 14:14:09 -0500

    Started Particles, Made Overtime Zone Scene

--- ConUHacksGodot/conuhacksvideogame/scripts/overtime_zone.gd
+++ ConUHacksGodot/conuhacksvideogame/scripts/overtime_zone.gd
@@ -9,3 +9,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass
+	#if area is Player1:
+		#print("Player 1 is here!")
+	#if area is Player2:
+		#print("Player 2 is here!")
+	#else
+		#print("Something entered here!)
