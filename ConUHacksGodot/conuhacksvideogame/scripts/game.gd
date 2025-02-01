ed9ae994ecb69a39ed64950e537f9cbe43199839
DanielaVU <davu2005@icloud.com>
2025-02-01 16:26:47 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- ConUHacksGodot/conuhacksvideogame/scripts/game.gd
+++ ConUHacksGodot/conuhacksvideogame/scripts/game.gd
@@ -3,15 +3,20 @@ extends Node2D
 @export var total_rounds = 0
 @export var player_one_rounds = 0
 @export var player_two_rounds = 0
+ 
+@onready var player_one_score: Node2D = $score1
+@onready var player_two_score: Node2D = $score2
 
 @onready var player_one_rounds_text: RichTextLabel = $UI/UI_Control/PlayerOneRoundsText
 @onready var player_two_rounds_text: RichTextLabel = $UI/UI_Control/PlayerTwoRoundsText
 
+var player1WINS = false
+var player2WINS = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -24,16 +29,18 @@ func player_one_round_won():
 	if (player_one_rounds >= 5):
 		print("Player One Won!")
 		player_one_rounds_text.text = "Player 1 Won!!!"
+		player_one_score.addtoScore1()
+		
 	else:
 		player_one_rounds_text.text = "Player 1 Rounds Won: " + str(player_one_rounds) #str() turns integer into a String
 		if (player_one_rounds % 2 == 0):
 			player_two_round_won()
 	
-
 func player_two_round_won():
 	player_two_rounds += 1
 	if (player_two_rounds >= 5):
 		print("Player Two Won!")
 		player_two_rounds_text.text = "Player 2 Won!!!"
+		player_two_score.addtoScore2()
 	else:
 		player_two_rounds_text.text = "Player 2 Rounds Won: " + str(player_two_rounds) #str() turns integer into a String
