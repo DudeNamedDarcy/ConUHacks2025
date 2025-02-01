1d053b43a7bf0d66ef686b3d37e901f50ce17841
CZhu <zhu.cathulhu@gmail.com>
2025-02-01 13:04:55 -0500

    Countdown1

--- ConUHacksGodot/conuhacksvideogame/scenes/Countdown.gd
+++ ConUHacksGodot/conuhacksvideogame/scenes/Countdown.gd
@@ -2,17 +2,26 @@ extends Node
 
 var  label = RichTextLabel
 var time = Timer
+@export var Short : Color
+@export var OgColor : Color
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	label = $RichTextLabel
 	time = $Timer
-	
+	OgColorRet()
 	time.start()
 	
 func _process(delta):
 	update_label_text()
-
+	if time.time_left <= 10:
+		label.modulate = Short
+	else:
+		OgColorRet()
+		
+func OgColorRet():
+	label.modulate = OgColor
+		
 func update_label_text():
 	label.text = str(ceil(time.time_left))
 
