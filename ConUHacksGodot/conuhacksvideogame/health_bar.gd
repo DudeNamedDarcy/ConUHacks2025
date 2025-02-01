d8dea7d936a39eb226173e9c9b30919ab07263c1
CZhu <zhu.cathulhu@gmail.com>
2025-02-01 15:12:51 -0500

    Heatlh2

--- ConUHacksGodot/conuhacksvideogame/health_bar.gd
+++ ConUHacksGodot/conuhacksvideogame/health_bar.gd
@@ -3,12 +3,8 @@ extends Node2D
 
 @export var max_health = 100
 var health = 100
-
-@export var LOW_HP : Color
-@export var Og_HP : Color
 var Health_label = RichTextLabel
 var HP_perc = ProgressBar
-
 
 func update_health_bar():
 	$Health_bar.value = health
@@ -18,16 +14,25 @@ func take_damage(damage):
 	update_health_bar()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	Health_label = $RichTextLabel
 	HP_perc = $Health_bar
+	
+	if health <= 25:
+		var r = abs(health * 0.01 - 1.0)
+		var g = health * 0.01
+		var b = 0.0
+		$Health_bar.modulate = Color(r, b, g)
+	
 	if health == 0:
 		print("You died!")
 		
 	if Input.get_action_strength("Damage"):
 		take_damage(1)
-		
+	
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health = max_health
 	update_health_bar()
+	
