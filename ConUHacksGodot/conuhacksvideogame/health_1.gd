662ec9949c9615b6eac11cb9827bd6df95df442e
CZhu <zhu.cathulhu@gmail.com>
2025-02-02 04:43:06 -0500

    iukj

--- ConUHacksGodot/conuhacksvideogame/health_1.gd
+++ ConUHacksGodot/conuhacksvideogame/health_1.gd
@@ -13,7 +13,11 @@ func update_health_bar(given_health):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	var temp = health_bar.value
 	health_bar.value = Global.player_one_health
+	
+	if temp < health_bar.value:
+		$AudioListener2D/PlayerHit.play()
 	
 	if $Health_bar.value > 25:
 		var g = 255
