eb28f58c2c6fed2a28ca29f4f2b68a14afa30b7a
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-02-02 09:09:55 -0500

    Somehow, Game Over Text Works!
    Changed global variables to int and it just works now

--- ConUHacksGodot/conuhacksvideogame/scenes/projectile.gd
+++ ConUHacksGodot/conuhacksvideogame/scenes/projectile.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 200
 
 var direction: float
 var spawnPosition: Vector2
--- ConUHacksGodot/conuhacksvideogame/scripts/game_over_screen.gd
+++ ConUHacksGodot/conuhacksvideogame/scripts/game_over_screen.gd
@@ -5,22 +5,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(Global.player_one_won)
+	print(Global.player_two_won)
 	Global.in_startup = false
 	declare_winner()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	declare_winner()
 	if Input.is_action_just_pressed("player1DOWN"):
 			Global.player_one_won = true
 
 func declare_winner():
-	if (Global.player_one_won == true):
+	if (Global.player_one_won == 10):
 		who_won_text.text = "[center] Player 1 Won! [/center]"
-	if (Global.player_two_won == true):
+	if (Global.player_two_won == 10):
 		who_won_text.text = "[center] Player 2 Won! [/center]"
-	else:
-		who_won_text.text = "[center] [winner shown here] [/center]"
 
 func _on_play_again_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/intermission.tscn")
