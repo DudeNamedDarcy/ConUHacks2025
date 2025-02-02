577405abe579dabbed6e8ebb208643680c06610b
DanielaVU <davu2005@icloud.com>
2025-02-02 02:24:53 -0500

    aaaaaaaaaaaaaaaaaaaaaaa

--- ConUHacksGodot/conuhacksvideogame/health_bar_1.gd
+++ ConUHacksGodot/conuhacksvideogame/health_bar_1.gd
@@ -6,14 +6,17 @@ var health = 100
 @onready var HP_perc = $Health_bar
 @onready var health_bar: ProgressBar = $Health_bar
 
-
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	health = max_health
+	update_health_bar(health)
+	$RichTextLabel.visible = false
 
 func update_health_bar(given_health):
 	$Health_bar.value = given_health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	Global.player_one_health = health_bar.value
 	
 	if health <= 25:
 		var r = abs(health * 0.01 - 1.0)
@@ -24,10 +27,3 @@ func _process(_delta: float) -> void:
 	if health == 0:
 		print("You died!")
 		$RichTextLabel.visible = true
-		
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	health = max_health
-	update_health_bar(health)
-	$RichTextLabel.visible = false
